a625adff48ae3c472c3027a8d951307a7fc3485c
Jarod <j.sengkeo@student.isartdigital.com>
2022-03-17 23:42:46 +0100

    added basic functions to vec<T, N>

--- include/math/types/vect.hpp
+++ include/math/types/vect.hpp
@@ -5,24 +5,100 @@
 template<typename T, int N>
 struct vec
 {
-	T elem[N];
+	union
+	{
+		T elem[N];
+	};
+
+	inline vec<T, N>() = default;
+
+	/**
+	 * Create a vector setting all of its value to the specified value.
+	 *
+	 * @param t
+	 */
+	inline vec<T, N>(const T& t);
+
+	/**
+	 * Get the square magnitude of this vector.
+	 *
+	 * @return
+	 */
+	inline float sqrMag() const;
+
+	/**
+	 * Get the magnitude of this vector.
+	 *
+	 * @return
+	 */
+	inline float mag() const;
+
+	/**
+	 * Get this vector with normalized magnitude.
+	 *
+	 * @return
+	 */
+	inline vec<T, N> normalized() const;
 
 	/**
 	 * Access this vector's elements with [].
-	 * 
+	 *
 	 * @param i
-	 * @return 
+	 * @return
 	 */
 	inline T& operator[](const uint i);
 
 	/**
 	 * Access this vector's elements with [].
-	 * 
+	 *
 	 * @param i
-	 * @return 
+	 * @return
 	 */
 	inline const T& operator[](const uint i) const;
 };
+
+template<typename T, int N>
+inline vec<T, N>::vec(const T& t)
+{
+	for (int i = 0; i < N; ++i)
+	{
+		elem[i] = t;
+	}
+}
+
+template<typename T, int N>
+inline float vec<T, N>::sqrMag() const
+{
+	float m = 0.f;
+	for (int i = 0; i < N; ++i)
+	{
+		m += (float)elem[i] * (float)elem[i];
+	}
+
+	return m;
+}
+
+template<typename T, int N>
+inline float vec<T, N>::mag() const
+{
+	return sqrtf(sqrMag());
+}
+
+template<typename T, int N>
+inline vec<T, N> vec<T, N>::normalized() const
+{
+	float m = mag();
+	if (m == 0.f)
+		return *this;
+
+	vec<T, N> v;
+	for (int i = 0; i < N; ++i)
+	{
+		v[i] = (float)elem[i] / m;
+	}
+
+	return v;
+}
 
 template<typename T, int N>
 inline T& vec<T, N>::operator[](const uint i)
--- src/main.cpp
+++ src/main.cpp
@@ -9,9 +9,11 @@ int main()
 	initMemleaksDebug();
 	std::cout << "Hello world !" << std::endl;
 
-	vec<uint, 10> v;
-	v[0] = 1;
-	v[2] = 21;
+	float3 f = { 1.f, 1.f, 1.f };
+	float3 fn = f.normalized();
+
+	vec<float, 3> v(1.f);
+	vec<float, 3> vn = v.normalized();
 
 	getchar();
 	return 0;
